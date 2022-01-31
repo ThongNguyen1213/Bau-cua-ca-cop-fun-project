d19bcaf8fde24bf003f82e0cc8812c3b6a141f18
Thong Nguyen <thongnguyen@Thongs-MacBook-Pro.local>
2022-01-31 01:39:31 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let sqr1 = document.getElementById('first-result');
+let sqr2 = document.getElementById('second-result');
+let sqr3 = document.getElementById('third-result');
+let button = document.getElementById('press');
+
+var arr = ["images/bau.jpg","images/ca.jpg","/images/cua.jpg","/images/ga.jpg","/images/nai.jpg","/images/tom.jpg"];
+
+function fortuneSelector() {
+    let randomNum1 = Math.floor(Math.random()*arr.length);
+    let randomNum2 = Math.floor(Math.random()*arr.length);
+    let randomNum3 = Math.floor(Math.random()*arr.length);
+    sqr1.setAttribute("src", arr[randomNum1]);
+    sqr2.setAttribute("src", arr[randomNum2]);
+    sqr3.setAttribute("src", arr[randomNum3]);
+}
+let changeText;
+function generateRandom() {
+    changeText = setInterval(fortuneSelector, 10);
+}
+function generateEnd(){
+    clearInterval(changeText);
+}
+
+button.addEventListener('mousedown', generateRandom);
+button.addEventListener('mouseup', generateEnd);
